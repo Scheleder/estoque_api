5e607242e805e42138eded476555d34e2fc36ac5
João Scheleder Neto <me@scheleder.com>
2024-06-12 21:27:41 -0300

    Implementado reset e alteração de senha

--- share/services/MailService.js
+++ share/services/MailService.js
@@ -1,33 +1,56 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const { MailtrapClient } = require("mailtrap");
 
 const TOKEN = process.env.MAIL_TOKEN;
 const ENDPOINT = process.env.MAIL_ENDPOINT;
 const FROM = process.env.MAIL_FROM;
 const APP = process.env.MAIL_APP;
-const sender = {  email: FROM,  name: APP};
+const sender = { email: FROM, name: APP };
 
 const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
 
-exports.sendCodeVerification = async(user)=>{
-  
+exports.sendCodeVerification = async (user) => {
+
   const recipients = [
     {
       email: user.email,
     }
   ];
-  
+
   client
     .send({
       from: sender,
       to: recipients,
       subject: APP,
-      text: "Olá "+user.name+"! Bem vindo ao "+APP+". Use o código "+user.code+" para confirmar seu endereço de e-mail.",
+      text: "Olá " + user.name + "! Bem vindo ao " + APP + ". Use o código " + user.code + " para confirmar seu endereço de e-mail.",
       category: "Welcome Email",
     })
-    .then(function(results){
+    .then(function (results) {
       console.log(results)
       return results.success;
-    });  
+    });
+
+}
+
+exports.sendResetCode = async (user) => {
+
+  const recipients = [
+    {
+      email: user.email,
+    }
+  ];
+
+  client
+    .send({
+      from: sender,
+      to: recipients,
+      subject: APP,
+      text: "Olá " + user.name + "! Se você solicitou a alteração de senha no " + APP + ". Use o código " + user.code + " para alterá-la.",
+      category: "Welcome Email",
+    })
+    .then(function (results) {
+      console.log(results)
+      return results.success;
+    });
 
 }
