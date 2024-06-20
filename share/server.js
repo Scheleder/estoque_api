015a1a86e71d14091e33e868268a5571698b0958
João Scheleder Neto <me@scheleder.com>
2024-06-20 20:31:54 -0300

    CORS ALLOW *

--- share/server.js
+++ share/server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const corsOptions ={
-  origin: `http://${host}`,
+  origin: '*',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
